0148f71380ea99aaf13665d697b4c27fc89b66ca
hattiee2003 <50850607+hattiee2003@users.noreply.github.com>
2020-09-09 12:31:00 +1200

    chess and book club pages
    chess has no information

--- src/routes/art.svelte
+++ src/routes/art.svelte
@@ -26,7 +26,7 @@ h2 {
 <h2 class="subtitle" id="where">Where: Art block</h2>
 <p>Senior Visual Art students provide learning opportunities to help you develop new art making skills, extend on what you are doing in class, help go over missed learning, encourage you to enter art events and competitions or support collaborative projects. Success in art comes from practice not talent! Come and have a go and make friends with fellow art enthusiasts in a warm and friendly environment! All welcome!</p>
 <br>
-<p>You do not have to have any particular skills to join. This club is free and open to all and you can join any time. Bring along some art to do and have some fun! </p>
+<p>You do not have to have any particular skills to join. This club is free and open to all and you can join any time. Bring along some art to do and have fun!</p>
 <br>
 <h2 class="subtitle" id="signUp">Just come along to try it out!</h2>
 <h2 class="subtitle" id="leader" style=" color: hsl(48, 100%, 67%);
