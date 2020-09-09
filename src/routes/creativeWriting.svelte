a015c9e58237ff5d80474962283af2d2d9c7ca56
Julia Wakeman <wakeman.j@rangiruru.school.nz>
2020-09-09 12:06:41 +1200

    update 5
    Added creative writing

--- src/routes/creativeWriting.svelte
+++ src/routes/creativeWriting.svelte
@@ -1,1 +1,33 @@
-<p>creative writing</p>
+<style>
+h1 {
+    text-align: center;
+    font-size: 50px;
+}
+
+h2 {
+    text-align: center;
+}
+
+#when {
+    color: hsl(204, 86%, 53%);
+}
+
+#where {
+    color: hsl(204, 86%, 53%);
+}
+
+#signUp {
+    color: hsl(48, 100%, 67%);
+}
+</style>
+
+<h1 class="title">Creative Writing Club</h1>
+<h2 class="subtitle" id="when">When: Wednesday lunchtimes</h2>
+<h2 class="subtitle" id="where">Where: English block E2</h2>
+<p>During creative writing workshops we develop ideas in a range of writing styles and genres. We share what we have written and offer supportive feedback. Students are also encouraged to attend other writing courses and enter a range of competitions.</p>
+<br>
+<p>You do not have to have any particular skills to join. This club is free and open to all. Bring along your prefered way of writing, for example your laptop or pen and paper. </p>
+<br>
+<h2 class="subtitle" id="signUp">Just come along to try it out!</h2>
+<h2 class="subtitle" id="leader" style=" color: hsl(48, 100%, 67%);
+">Leader: Ms Bovett</h2>
