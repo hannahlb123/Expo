8c1b1bc4ba8cbbaac93cea33b2e0f39da3746792
Julia Wakeman <wakeman.j@rangiruru.school.nz>
2020-09-15 10:14:11 +1200

    update

--- src/routes/jazzBand.svelte
+++ src/routes/jazzBand.svelte
@@ -1,1 +1,41 @@
-<p>jazz band</p>
+<style>
+h1 {
+    text-align: center;
+    font-size: 50px;
+}
+
+h2 {
+    text-align: center;
+}
+
+#when {
+    color: hsl(204, 86%, 53%);
+}
+
+#where {
+    color: hsl(204, 86%, 53%);
+}
+
+#signUp {
+    color: hsl(48, 100%, 67%);
+}
+
+img {
+  width: 40%;
+  height: auto;
+}
+</style>
+
+<h1 class="title">Jazz Band</h1>
+<h2 class="subtitle" id="when">When: Fridays 3:45 - 5:30</h2>
+<h2 class="subtitle" id="where">Where: Music Block</h2>
+<p>You will learn about playing jazz and improvising.  Prepare for lots of gigs - at school, in the community - Big Band Festival, Southern Jam, play at cafes/bars/clubs, prepare for competitions.</p>
+<br>
+<p>You must be able to read music, and must be learning your instrument.  An audition is required.  Open to saxophones, trumpets and trombones. Very limited space for a competent pianist, competent drummer, bass player and rhythm guitar - there are not spaces for these players every year. The cost is around $500 a year to travel to Blenheim for the Southern Jam Youth Jazz Festival. All year groups are welcome!</p>
+<br>
+<img src="jazz1.jpeg" alt="jazz band at sourthen jazz">
+<img src="jazz2.jpeg" alt="jazz band practise">
+<img src="jazz3.jpeg" alt="jazz band performing at prizegiving">
+<h2 class="subtitle" id="signUp">Sign ups are currently closed. Check back at the beggining of the year.</h2>
+<h2 class="subtitle" id="leader" style=" color: hsl(48, 100%, 67%);
+">Leader: Lana Law</h2>
