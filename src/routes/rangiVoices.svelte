f920a395f8a514e0b950e5b3cde5ddd68c82b951
hattiee2003 <50850607+hattiee2003@users.noreply.github.com>
2020-10-14 09:17:34 +1300

    all pages complete

--- src/routes/rangiVoices.svelte
+++ src/routes/rangiVoices.svelte
@@ -1,1 +1,35 @@
-<p>Rangi voices</p>
+<style>
+h1 {
+    text-align: center;
+    font-size: 50px;
+}
+
+h2, p {
+    text-align: center;
+}
+
+#when {
+    color: hsl(204, 86%, 53%);
+}
+
+#where {
+    color: hsl(204, 86%, 53%);
+}
+
+#signUp {
+    color: hsl(48, 100%, 67%);
+}
+
+
+</style>
+
+<h1 class="title">Rangi Voices</h1>
+<h2 class="subtitle" id="when">When: Friday mornings 8:00-9:00</h2>
+<h2 class="subtitle" id="where">Where: Music Block</h2>
+<p><b>Years 7-10</b> rehearse together once a week to learn about choir singing through a range of songs. Rangi Voices gives performances at school events (e.g. Clan and Carol services), sings in the community - e.g with choirs from other Independant Schools - and takes part in the annual national Big Sing competition.</p>
+<br>
+<p>They need to enjoy singing together and learn about what it means to be a positive and contributing member of a choir. It is not necessary to have singing lessons or necessary to be able to read music, it is an advantage though.</p>
+<br>
+<h2 class="subtitle" id="signUp">Sign up for 2021 by 2/5/21</h2>
+<h2 class="subtitle" id="leader" style=" color: hsl(48, 100%, 67%);
+">Leader: Mrs Charlton</h2>
