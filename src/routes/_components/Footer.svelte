9ccf72385c3ecb26ac1802d6780c3742bf2d56df
hattiee2003 <50850607+hattiee2003@users.noreply.github.com>
2020-11-02 13:06:32 +1300

    update

--- src/routes/_components/Footer.svelte
+++ src/routes/_components/Footer.svelte
@@ -1,9 +1,7 @@
 <footer class="footer">
   <div class="content has-text-centered">
     <p>
-      <strong>Expo</strong> by Julia Wakeman and Harriet Eglinton. The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-      is licensed Rangi Ruru Girls School.
+      <strong>Expo</strong> by Julia Wakeman and Harriet Eglinton. The source code is licensed by <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed by Rangi Ruru Girls School.
     </p>
   </div>
 </footer>
